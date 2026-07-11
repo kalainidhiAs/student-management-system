a3f915a6e04a788dbc50e1959852064e0f506fe8
kalainidhiAs <117141474+kalainidhiAs@users.noreply.github.com>
2026-07-11 23:19:32 +0530

    all crud operatioin and pagination is done

--- src/main/java/com/student_management_system/sms/controller/StudentController.java
+++ src/main/java/com/student_management_system/sms/controller/StudentController.java
@@ -8,9 +8,17 @@ import com.student_management_system.sms.service.StudentService;
 
 import jakarta.validation.Valid;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/students")
@@ -25,6 +33,33 @@ public class StudentController {
     @PostMapping("/create")
     public Student createStudent(@Valid @RequestBody Student student) {
         return studentService.createStudent(student);
+    }
+
+    @GetMapping("/getAllStudents")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/getStudent/{id}")
+    public String getMethodName(@PathVariable Long id) {
+        return studentService.getStudentById(id);
+    }
+
+    @PutMapping("updateStudent/{id}")
+    public Student putMethodName(@PathVariable Long id, @RequestBody Student student) {
+        return studentService.updateStudent(id, student);
+    }
+
+    @DeleteMapping("/deleteStudent/{id}")
+    public void deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+    }
+
+    @GetMapping("/all")
+    public Page<Student> getStudentByPage(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size) {
+        return studentService.getStudentsByPage(page, size);
     }
 
 }
--- src/main/java/com/student_management_system/sms/service/StudentService.java
+++ src/main/java/com/student_management_system/sms/service/StudentService.java
@@ -1,5 +1,10 @@
 package com.student_management_system.sms.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.student_management_system.sms.model.Student;
@@ -17,4 +22,41 @@ public class StudentService {
     public Student createStudent(Student student) {
         return studentRepository.save(student);
     }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public String getStudentById(Long id) {
+        return studentRepository.findById(id).map(student -> student.toString())
+                .orElse("Not Found");
+    }
+
+    public Student updateStudent(Long id, Student student) {
+        Student existingStudent = studentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Student not found"));
+
+        existingStudent.setFirstName(student.getFirstName());
+        existingStudent.setLastName(student.getLastName());
+        existingStudent.setPhone(student.getPhone());
+        existingStudent.setEmail(student.getEmail());
+        existingStudent.setDob(student.getDob());
+        existingStudent.setDepartment(student.getDepartment());
+        existingStudent.setCgpa(student.getCgpa());
+        existingStudent.setDoj(student.getDoj());
+
+        return studentRepository.save(existingStudent);
+
+    }
+
+    public void deleteStudent(Long id) {
+        studentRepository.findById(id).orElseThrow(() -> new RuntimeException("Student not found"));
+        studentRepository.deleteById(id);
+    }
+
+    public Page<Student> getStudentsByPage(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return studentRepository.findAll(pageable);
+    }
+
 }
